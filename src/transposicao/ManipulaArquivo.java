8fe9a67efd04f8c0dd514ac8ceba5a2a56bd4938
Lucas Lima <lucaslz@rocketmail.com>
2017-10-16 01:46:21 -0300

    primeiro commit, o projeto ainda não esta totalmente pronto;

---
+++ src/transposicao/ManipulaArquivo.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transposicao;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import static jdk.nashorn.internal.objects.NativeError.printStackTrace;
+
+/**
+ *
+ * @author Lucas
+ */
+public class ManipulaArquivo {
+    
+    private String nome;
+    private File arquivo;
+    private BufferedReader ler;
+    private BufferedWriter escrever;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public File getArquivo() {
+        setarArquivo();
+        return arquivo;
+    }
+    
+    public void setarArquivo(){
+        this.arquivo = new File(getNome());
+    }
+
+    public BufferedReader getLer() {
+        return ler;
+    }
+
+    public void setLer(BufferedReader ler) {
+        this.ler = ler;
+    }
+
+    public BufferedWriter getEscrever() {
+        return escrever;
+    }
+
+    public void setEscrever(BufferedWriter escrever) {
+        this.escrever = escrever;
+    }
+    
+    public boolean verificaSeArquivoExiste()
+    {   
+        if (getArquivo().exists()) {
+            return false;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean criaArquivo()
+    {
+        try {
+            getArquivo().createNewFile();
+            return true;
+        } catch (IOException ex) {
+            printStackTrace("Não foi possivel criar o arquivo");
+            return false;
+        }
+    }
+    
+    public boolean escerver(String palavra)
+    {
+        if (verificaSeArquivoExiste()) {
+            try {
+                setEscrever(new BufferedWriter(new FileWriter(getArquivo())));
+                getEscrever().append(palavra);
+                getEscrever().flush();
+                getEscrever().close();
+                return true;
+            } catch (IOException e) {
+                printStackTrace("Não foi possivel escrever no arquivo.");
+                return false;
+            }
+        }
+        return false;
+    }
+    
+    public ArrayList ler()
+    {
+        Scanner lerArquivo;
+        ArrayList arrayFrases = new ArrayList();
+        
+        if (verificaSeArquivoExiste()) {
+            try {
+                setLer(new BufferedReader(new FileReader(getArquivo())));
+                lerArquivo = new Scanner(getLer());
+                while (lerArquivo.hasNextLine()) {                    
+                    arrayFrases.add(lerArquivo.nextLine());
+                }
+            } catch (IOException ex) {
+                printStackTrace("Não foi possivel ler o arquivo.");
+            }
+        }
+        
+        return arrayFrases;
+    }
+}
---
+++ src/transposicao/Transposicao.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transposicao;
+import java.util.Scanner;
+/**
+ *
+ * @author nobreack
+ */
+public class Transposicao {
+    
+    public static String entradaFrase = "frase.txt";
+    public static String entradaChave = "chave.txt";    
+    public static String saidaCriptografada = "saida.txt";
+    public static String frase;
+    public static String chave;
+    public static char[][] matrizTransposicao;
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Pegando a frase do usuario
+        System.out.print("Digite uma frase: ");
+        Scanner lerPalavra = new Scanner(System.in);
+        String palavra = lerPalavra.nextLine();
+        setFrase(palavra);
+        
+        //Pegando a palavra chave do usuario
+        System.out.print("Digite uma chave: ");
+        Scanner lerChave = new Scanner(System.in);
+        String palavraChave = lerChave.nextLine();
+        setChave(palavraChave);
+
+        ManipulaArquivo arquivo = new ManipulaArquivo();
+        
+        //Escrever frase no arquivo
+        arquivo.setNome(getEntradaFrase());
+        arquivo.escerver(getFrase());
+        
+        //Escrever chave no arquivo
+        arquivo.setNome(getEntradaChave());
+        arquivo.escerver(getChave());
+        
+        //Transpondo a frase
+        transporFrase();
+        
+    }
+
+    public static String getEntradaFrase() {
+        return entradaFrase;
+    }
+
+    public static String getEntradaChave() {
+        return entradaChave;
+    }
+
+    public static String getSaidaCriptografada() {
+        return saidaCriptografada;
+    }
+
+    public static String getFrase() {
+        return frase;
+    }
+
+    public static void setFrase(String frase) {
+        Transposicao.frase = frase;
+    }
+
+    public static String getChave() {
+        return chave;
+    }
+
+    public static char[][] getMatrizTransposicao() {
+        return matrizTransposicao;
+    }
+
+    public static void setMatrizTransposicao(char[][] matrizTransposicao) {
+        Transposicao.matrizTransposicao = matrizTransposicao;
+    }
+
+    public static void setChave(String chave) {
+        Transposicao.chave = chave;
+    }
+    
+    public static void transporFrase()
+    {
+        //Definindo valor dos Fors que iram rodar
+        int totalFrase = getFrase().length();
+        int totalChave = getChave().length();
+        int posicaoAtual = 0;
+        int linha = 1;
+        StringBuilder string = new StringBuilder();
+        
+        //Setando matriz de transposicao
+        setMatrizTransposicao(new char[100][totalChave]);
+        
+        //Colocando a chave na cabeca do array
+        for (int i = 0; i < totalChave; i++) {
+            getMatrizTransposicao()[0][i] = getChave().charAt(i);
+        }
+        
+        //Colocando frase na matriz de transposicao
+        while(totalFrase > posicaoAtual) {
+            for (int i = 0; i < totalChave; i++) {
+                if (totalFrase == posicaoAtual) {
+                    posicaoAtual--;
+                }
+                getMatrizTransposicao()[linha][i] = getFrase().charAt(posicaoAtual);                    
+                posicaoAtual++;                    
+                
+            }
+           
+            if (posicaoAtual >= totalChave) {
+                linha++;
+            }
+        }
+        
+        for (int i = 0; i < totalChave; i++) {
+            for (int j = 0; j < totalChave; j++) {
+                if (j != 0) {
+                    string.append(getMatrizTransposicao()[j][i]);    
+                }
+                System.out.print(getMatrizTransposicao()[i][j]);
+            }
+            System.out.println("");
+        }
+        
+        String palavraCompleta = string.toString();
+        System.out.println("\n" + palavraCompleta);
+    }
+}
